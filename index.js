ebdc310b7e587914ee91b62d3a58926b02164f66
github username <github email address>
2020-05-20 12:08:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions here
+
+function map(sourceArray, func){
+    const newArray = []
+
+    for (let i = 0; i < sourceArray.length; i++){
+        newArray.push(func(sourceArray[i]))
+    }
+
+    return newArray
+}
+
+function reduce(sourceArray, func, startingPoint){
+
+    let total // let doesnt need to be assigned when declared
+    let i = 0
+    //setting counter to 0 works for all but reduce that deals with truthiness
+    //0 is falsy
+    //so then can start with first element in array
+    if (startingPoint){
+        total = startingPoint
+    } else {
+        total = sourceArray[0] 
+        i++ //to skip first element in loop
+    }
+
+    for (i; i < sourceArray.length; i++){
+        total = func(sourceArray[i], total) 
+        //counter = not += b/c going to operate on counter in the function, 
+        // + will happen in callback function
+    }
+
+    return total
+}
